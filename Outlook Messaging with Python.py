a56c5c6bdd6e5b122b7f2a208c00715d27d999e7
Atharv Rege <74661742+Athu1712@users.noreply.github.com>
2021-04-12 11:52:55 +0530

    Add files via upload

---
+++ Outlook Messaging with Python.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+port = 587  # For starttls
+smtp_server = "smtp-mail.outlook.com"
+sender_email = "Youremail@outlook.com"
+receiver_email = "reciver@outlook.com"
+password = input("Type your password and press enter:")
+message = """\
+Subject: Your subject
+
+Your multiline text here
+"""
+
+context = ssl.create_default_context()
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()  # Can be omitted
+    server.starttls(context=context)
+    server.ehlo()  # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
